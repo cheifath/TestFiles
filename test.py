c6763b9e93e7f41005f950030feb4963d98dac94
Cheifath <cheifathmohammed@gmail.com>
2026-02-19 12:09:42 +0530

    No Error

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-import subprocess
+def add(a, b):
+    return a + b
 
-def very_bad_function(a, b, c, d, e, f):
-    if a:
-        if b:
-            if c:
-                if d:
-                    if e:
-                        if f:
-                            print("Too deep")
-
-subprocess.call("ls", shell=True)
