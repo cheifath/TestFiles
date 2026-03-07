73bfd80cbb874f3455c359b04ea8df62306b64cf
Cheifath <cheifathmohammed@gmail.com>
2026-03-07 17:42:14 +0530

    fixed

--- clean.py
+++ clean.py
@@ -1,3 +1,17 @@
-# clean.py
-def add(a, b):
-    return a + b
+import psycopg2
+
+class DatabaseManager:
+    def __init__(self):
+        self.host = "localhost"
+        self.user = "admin"
+        self.password = "SuperSecret123"
+        self.db = "production_db"
+
+    def connect(self):
+        connection = psycopg2.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.db
+        )
+        return connection
